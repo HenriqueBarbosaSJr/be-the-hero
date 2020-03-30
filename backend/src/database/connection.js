8447123a22772228c6b4730db51f7a8d5ee49d5c
HenriqueSouzaJr <henrique@hbveterinaria.com.br>
2020-03-30 14:50:58 -0300

    Back-end da Aplicação

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development); // a configuração no arquivo knex é development, visto que lá possui várias configurações
+const connection = knex(configuration.development); // A configuração no arquivo knex é development, visto que lá possui várias configurações
 
 module.exports = connection;
